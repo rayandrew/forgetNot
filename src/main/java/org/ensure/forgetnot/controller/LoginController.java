ff45ea578f0eddc08b370447d041d7724e806923
aldrichvalentino <aldrich.vh97@gmail.com>
2017-04-24 17:55:14 +0700

    asdf

--- src/main/java/org/ensure/forgetnot/controller/LoginController.java
+++ src/main/java/org/ensure/forgetnot/controller/LoginController.java
@@ -1,9 +1,9 @@
 package org.ensure.forgetnot.controller;
 
+import org.ensure.forgetnot.core.Controller;
+
 import java.awt.Button;
 import java.awt.Component;
-
-import org.ensure.forgetnot.core.Controller;
 
 /**
  * Created by rufus on 4/22/2017.
