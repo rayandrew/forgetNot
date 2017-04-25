8c78411e8e83ded2212aed5b1b8158f63bab0894
aldrichvalentino <aldrich.vh97@gmail.com>
2017-04-25 08:14:20 +0700

    added dialog box

--- src/main/java/org/ensure/forgetnot/view/ActivityView.java
+++ src/main/java/org/ensure/forgetnot/view/ActivityView.java
@@ -2,10 +2,14 @@ package org.ensure.forgetnot.view;
 
 import com.alee.laf.text.WebTextField;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import java.awt.Button;
 import java.awt.Component;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by rufus on 4/23/2017.
@@ -14,14 +18,16 @@ public class ActivityView extends View {
   private Panel activityPanel;
   private JTable tab;
   private WebTextField textPane;
+  private Button addActivityButton;
 
   public ActivityView(int numOfActivities) {
     super("activityView");
     //System.out.println(numOfActivities);
 
     activityPanel = new Panel();
-    textPane = new WebTextField();
-
+    textPane = new WebTextField(null,10);
+    addActivityButton = new Button("Add a Reminder");
+    addActivityButton.addActionListener(new Action());
 
     String[] columns = {"Activity", "Time"};
     Object[][] data = {{"Memasak", "20.20"}, {"Memasak", "20.20"}, {"Memasak", "20.20"}, {"Memasak", "20.20"}};
@@ -30,14 +36,23 @@ public class ActivityView extends View {
 
     activityPanel.add(tab);
     activityPanel.add(textPane);
+    activityPanel.add(addActivityButton);
+
+    //TODO: buat array untuk simpan masukan pengguna
   }
 
   public void addWebContent(DefaultTableModel table) {
     tab.setModel(table);
   }
 
-  public void getValueInput() {
-
+  //Action Listener For Button Click
+  static class Action implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      JOptionPane.showMessageDialog(null, "Please input your activity",
+          "Add a Reminder", JOptionPane.INFORMATION_MESSAGE);
+      String input1 = JOptionPane.showInputDialog(null,"Input the Reminder Title");
+    }
   }
 
   @Override
