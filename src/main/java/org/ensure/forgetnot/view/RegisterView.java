71854e67f3862494b7096a3f892f17668f702cad
girvandip <girvandip@gmail.com>
2017-04-26 04:08:39 +0700

    register view udh lah ya

--- src/main/java/org/ensure/forgetnot/view/RegisterView.java
+++ src/main/java/org/ensure/forgetnot/view/RegisterView.java
@@ -6,19 +6,27 @@ import com.alee.extended.panel.GroupPanel;
 import com.alee.laf.WebLookAndFeel;
 import com.alee.laf.button.WebButton;
 import com.alee.laf.label.WebLabel;
+import com.alee.laf.optionpane.WebOptionPane;
 import com.alee.laf.panel.WebPanel;
 import com.alee.laf.rootpane.WebDialog;
+import com.alee.laf.rootpane.WebFrame;
+import com.alee.laf.scroll.WebScrollPane;
 import com.alee.laf.text.WebPasswordField;
 import com.alee.laf.text.WebTextField;
 import com.alee.managers.hotkey.Hotkey;
 import com.alee.managers.hotkey.HotkeyManager;
 import com.alee.utils.SwingUtils;
+import org.ensure.forgetnot.core.Database;
+import org.ensure.forgetnot.model.User;
 import org.ensure.forgetnot.utility.Pair;
 
 import javax.swing.JSeparator;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -26,12 +34,13 @@ import java.util.List;
  */
 public class RegisterView extends View {
   Dialog dialog;
-  WebButton test = new WebButton();
+  WebButton test;
+
   public RegisterView(String componentName) {
     super(componentName);
-    Dialog dialog = new Dialog(new Button());
-    dialog.pack();
-    dialog.setVisible(true);
+    //Dialog dialog = new Dialog(new Button());
+    test = new WebButton("Register");
+    test.addActionListener(new Dialog());
   }
 
   public RegisterView(String componentName, String componentDescription) {
@@ -49,48 +58,195 @@ public class RegisterView extends View {
     super(dataInput);
   }
 
-  private class Dialog extends WebDialog {
-    public Dialog(Button owner) {
+  private class Dialog implements ActionListener{
+    private String[] arr = new String[5];
+    public Dialog() {
+      super();
+      arr[0] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Username",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[1] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Password",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[2] = WebOptionPane.showInputDialog(
+        null,
+        "Enter First Name",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[3] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Last Name",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[4] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Email",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+
+      String timeStamp = LocalDateTime.now().format(
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+      );
+
+      Database.connect();
+      for(String a : arr){
+        System.out.println(a);
+      }
+      User.createUser(arr[0],arr[1],arr[2],arr[3],arr[4], timeStamp,arr[0]);
+      Database.close();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+  }
+
+/*
+  private class Dialog1 extends WebDialog {
+    public Dialog1(Button owner) {
       super(owner, "Register");
       setDefaultCloseOperation(WebDialog.DISPOSE_ON_CLOSE);
-      setResizable(false);
+      setResizable(true);
       setModal(true);
+
       TableLayout layout = new TableLayout(new double[][]{{TableLayout.PREFERRED, TableLayout.FILL},
         {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}});
       layout.setHGap(5);
       layout.setVGap(5);
-      WebPanel content = new WebPanel(layout);
-      content.setMargin(15, 30, 15, 30);
-      content.setOpaque(false);
-      content.add(new WebLabel("Username", WebLabel.TRAILING), "0,0");
+      WebPanel content1 = new WebPanel(layout);
+
+      content1.setMargin(15, 30, 15, 30);
+      content1.setOpaque(false);
+      content1.add(new WebLabel("Username", WebLabel.TRAILING), "0,0");
       WebTextField username = new WebTextField(15);
       username.setInputPrompt("Enter Username..");
       username.setInputPromptFont ( username.getFont ().deriveFont ( Font.ITALIC ) );
-      content.add(username, "1,0");
-      content.add(new WebLabel("Password", WebLabel.TRAILING), "0,1");
+      content1.add(username, "1,0");
+      content1.add(new WebLabel("Password", WebLabel.TRAILING), "0,1");
       WebPasswordField password = new WebPasswordField(15);
       password.setInputPrompt("Enter Password..");
       password.setInputPromptFont ( password.getFont ().deriveFont ( Font.ITALIC ) );
-      content.add(password, "1,1");
-      WebButton login = new WebButton("Register");
-      WebButton cancel = new WebButton("Cancel");
+      content1.add(password, "1,1");
+      WebButton next = new WebButton("Next");
+      ActionListener listener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          Dialog2 dialog2 = new Dialog2(new Button());
+        }
+      };
+      next.addActionListener(listener);
+      content1.add(new CenterPanel(new GroupPanel(5, next)), "0,2");
+      add(content1);
+      center();
+    }
+  }
+
+  private class Dialog2 extends WebDialog {
+    public Dialog2(Button owner) {
+      super(owner,"Register");
+      String[] arr = new String[5];
+      arr[0] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Username",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[1] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Password",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[2] = WebOptionPane.showInputDialog(
+        null,
+        "Enter First Name",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[3] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Last Name",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+      arr[4] = WebOptionPane.showInputDialog(
+        null,
+        "Enter Email",
+        "Register",
+        WebOptionPane.QUESTION_MESSAGE);
+
+
+      setDefaultCloseOperation(WebDialog.DISPOSE_ON_CLOSE);
+      setResizable(true);
+      setModal(true);
+
+      TableLayout layout = new TableLayout(new double[][]{{TableLayout.PREFERRED, TableLayout.FILL},
+        {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}});
+      layout.setHGap(5);
+      layout.setVGap(5);
+      WebPanel content1 = new WebPanel(layout);
+
+      content1.setMargin(15, 30, 15, 30);
+      content1.setOpaque(false);
+      content1.add(new WebLabel("First Name", WebLabel.TRAILING), "0,0");
+      WebTextField firstName = new WebTextField(15);
+      firstName.setInputPrompt("Enter First Name..");
+      firstName.setInputPromptFont ( firstName.getFont ().deriveFont ( Font.ITALIC ) );
+      content1.add(firstName, "1,0");
+      content1.add(new WebLabel("Last Name", WebLabel.TRAILING), "0,1");
+      WebTextField lastName = new WebTextField(15);
+      lastName.setInputPrompt("Enter Last Name..");
+      lastName.setInputPromptFont ( lastName.getFont ().deriveFont ( Font.ITALIC ) );
+      content1.add(lastName, "1,1");
+      WebButton next = new WebButton("Next");
+      ActionListener listener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          Dialog3 dialog3 = new Dialog3(new Button());
+        }
+      };
+      next.addActionListener(listener);
+      content1.add(new CenterPanel(new GroupPanel(5, next)), "0,2");
+      add(content1);
+      center();
+
+    }
+  }
+
+  private class Dialog3 extends WebDialog {
+    public Dialog3(Button owner) {
+      setDefaultCloseOperation(WebDialog.DISPOSE_ON_CLOSE);
+      setResizable(true);
+      setModal(true);
+
+      TableLayout layout = new TableLayout(new double[][]{{TableLayout.PREFERRED, TableLayout.FILL},
+        {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}});
+      layout.setHGap(5);
+      layout.setVGap(5);
+      WebPanel content1 = new WebPanel(layout);
+
+      content1.setMargin(15, 30, 15, 30);
+      content1.setOpaque(false);
+      content1.add(new WebLabel("Email", WebLabel.TRAILING), "0,0");
+      WebTextField email = new WebTextField(15);
+      email.setInputPrompt("Enter Email..");
+      email.setInputPromptFont ( email.getFont ().deriveFont ( Font.ITALIC ) );
+      content1.add(email, "1,0");
+      WebButton register = new WebButton("Register");
       ActionListener listener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
           setVisible(false);
         }
       };
-      login.addActionListener(listener);
-      cancel.addActionListener(listener);
-      content.add(new CenterPanel(new GroupPanel(5, login, cancel)), "0,2,1,2");
-      SwingUtils.equalizeComponentsWidths(login, cancel);
-      add(content);
-      HotkeyManager.registerHotkey(this, login, Hotkey.ESCAPE);
-      HotkeyManager.registerHotkey(this, login, Hotkey.ENTER);
+      register.addActionListener(listener);
+      content1.add(new CenterPanel(new GroupPanel(5, register)), "0,2");
+      add(content1);
       center();
     }
   }
-
+  */
   @Override
   public Component init() {
     return test;
