da18bdcdeb1781cea2f3123e894be65aa09a10c2
aldrichvalentino <aldrich.vh97@gmail.com>
2017-04-26 07:14:21 +0700

    java doc

--- src/main/java/org/ensure/forgetnot/core/DatabaseDaemonException.java
+++ src/main/java/org/ensure/forgetnot/core/DatabaseDaemonException.java
@@ -1,21 +1,37 @@
 package org.ensure.forgetnot.core;
 
 /**
- * Created by rufus on 4/13/2017.
+ * @author Ray
  */
 public class DatabaseDaemonException extends Exception {
+  /**
+   * Konstruktor tanpa parameter
+   * */
   public DatabaseDaemonException() {
     super();
   }
 
+  /**
+   * Kontruktor dengan parameter
+   * @param message pesan error
+   * @param throwable Kelas Exception
+   * */
   public DatabaseDaemonException(String message, Throwable throwable) {
     super(message, throwable);
   }
 
+  /**
+   * Konstukror
+   * @param message pesan error
+   * */
   public DatabaseDaemonException(String message) {
     super(message);
   }
 
+  /**
+   * Konstruktor
+   * @param throwable Kelas Exception
+   * */
   public DatabaseDaemonException(Throwable throwable) {
     super(throwable);
   }
